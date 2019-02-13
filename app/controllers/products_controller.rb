206bba7b848183472ec2c569ea978c42529c49a9
JulianaCamposP <jcampos@go-labs.net>
2019-02-13 14:14:49 -0600

    update and delete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,29 +1,47 @@
 class ProductsController < ApplicationController
-    def index
-        @products = Product.all     
-    end
-    def new
-        @product = Product.new
-    end
-    def create
-      @product = Product.new(product_params) 
-      respond_to do |format|
-        if @product.save
-          format.html { redirect_to products_path, notice: 'Product was successfully created.' }
-          format.json { render :show, status: :created, location: @product }
-        else
-          format.html { render :new }
-          format.json { render json: @product.errors, status: :unprocessable_entity }
-        end
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  def index
+      @products = Product.all     
+  end
+  def new
+      @product = Product.new
+  end
+  def create
+    @product = Product.new(product_params) 
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to products_path, notice: 'Product was successfully created.' }
+      else
+        format.html { render :new }
       end
     end
+  end
 
+  def edit
+  end
 
-    private
-    def set_product
-      @product = Product.find(params[:id])
+  def update
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to products_path, notice: 'Post was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
     end
-    def product_params
-      params.require(:product).permit(:name, :description)
+  end
+
+  def destroy
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_path, notice: 'Post was successfully destroyed.' }
     end
+  end
+
+  private
+  def set_product
+    @product = Product.find(params[:id])
+  end
+  def product_params
+    params.require(:product).permit(:name, :description)
+  end
 end
